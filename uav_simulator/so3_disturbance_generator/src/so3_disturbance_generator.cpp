df8bdff0ca983123f2d703b324b26a9b6e929913
damiankryzia71 <damiankryzia71@gmail.com>
2025-08-07 21:04:27 -0700

    update so3_disturbance_generator and pose_utils to ros2

--- uav_simulator/so3_disturbance_generator/src/so3_disturbance_generator.cpp
+++ uav_simulator/so3_disturbance_generator/src/so3_disturbance_generator.cpp
@@ -1,55 +1,109 @@
 #include <iostream>
-#include <string.h>
-#include <ros/ros.h>
-#include <geometry_msgs/Vector3.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <dynamic_reconfigure/server.h>
-#include <so3_disturbance_generator/DisturbanceUIConfig.h>
-#include "pose_utils.h"
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include "pose_utils/pose_utils.h"
 
 using namespace arma;
 using namespace std;
 
 #define CORRECTION_RATE 1
 
-ros::Publisher pubo;
-ros::Publisher pubc;
-ros::Publisher pubf;
-ros::Publisher pubm;
-so3_disturbance_generator::DisturbanceUIConfig config;
-nav_msgs::Odometry noisy_odom;
-geometry_msgs::PoseStamped correction;
+rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pubo;
+rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pubc;
+rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr pubf;
+rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr pubm;
+nav_msgs::msg::Odometry noisy_odom;
+geometry_msgs::msg::PoseStamped correction;
 
-void odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
+struct Config {
+  double fxy, fz;
+  double stdfxy, stdfz;
+  double mrp, myaw;
+  double stdmrp, stdmyaw;
+  double vdriftx, vdrifty, vdriftz, vdriftyaw;
+  double stdvdriftxyz, stdvdriftyaw;
+  double stdxyz, stdvxyz, stdrp, stdyaw;
+  bool enable_drift_odom;
+  bool enable_noisy_odom;
+} config;
+
+void declare_and_load_params(rclcpp::Node::SharedPtr node) {
+  node->declare_parameter("fxy", 0.0);
+  node->declare_parameter("fz", 0.0);
+  node->declare_parameter("stdfxy", 0.0);
+  node->declare_parameter("stdfz", 0.0);
+  node->declare_parameter("mrp", 0.0);
+  node->declare_parameter("myaw", 0.0);
+  node->declare_parameter("stdmrp", 0.0);
+  node->declare_parameter("stdmyaw", 0.0);
+  node->declare_parameter("vdriftx", 0.0);
+  node->declare_parameter("vdrifty", 0.0);
+  node->declare_parameter("vdriftz", 0.0);
+  node->declare_parameter("vdriftyaw", 0.0);
+  node->declare_parameter("stdvdriftxyz", 0.0);
+  node->declare_parameter("stdvdriftyaw", 0.0);
+  node->declare_parameter("stdxyz", 0.0);
+  node->declare_parameter("stdvxyz", 0.0);
+  node->declare_parameter("stdrp", 0.0);
+  node->declare_parameter("stdyaw", 0.0);
+  node->declare_parameter("enable_drift_odom", true);
+  node->declare_parameter("enable_noisy_odom", false);
+
+  node->get_parameter("fxy", config.fxy);
+  node->get_parameter("fz", config.fz);
+  node->get_parameter("stdfxy", config.stdfxy);
+  node->get_parameter("stdfz", config.stdfz);
+  node->get_parameter("mrp", config.mrp);
+  node->get_parameter("myaw", config.myaw);
+  node->get_parameter("stdmrp", config.stdmrp);
+  node->get_parameter("stdmyaw", config.stdmyaw);
+  node->get_parameter("vdriftx", config.vdriftx);
+  node->get_parameter("vdrifty", config.vdrifty);
+  node->get_parameter("vdriftz", config.vdriftz);
+  node->get_parameter("vdriftyaw", config.vdriftyaw);
+  node->get_parameter("stdvdriftxyz", config.stdvdriftxyz);
+  node->get_parameter("stdvdriftyaw", config.stdvdriftyaw);
+  node->get_parameter("stdxyz", config.stdxyz);
+  node->get_parameter("stdvxyz", config.stdvxyz);
+  node->get_parameter("stdrp", config.stdrp);
+  node->get_parameter("stdyaw", config.stdyaw);
+  node->get_parameter("enable_drift_odom", config.enable_drift_odom);
+  node->get_parameter("enable_noisy_odom", config.enable_noisy_odom);
+}
+
+void odom_callback(const nav_msgs::msg::Odometry& msg)
 {
-  noisy_odom.header = msg->header;  
-  correction.header = msg->header;  
+  noisy_odom.header = msg.header;  
+  correction.header = msg.header;  
   // Get odom
   colvec pose(6);
   colvec vel(3);  
-  pose(0)        = msg->pose.pose.position.x;
-  pose(1)        = msg->pose.pose.position.y;
-  pose(2)        = msg->pose.pose.position.z;    
+  pose(0)        = msg.pose.pose.position.x;
+  pose(1)        = msg.pose.pose.position.y;
+  pose(2)        = msg.pose.pose.position.z;    
   colvec q       = zeros<colvec>(4);
-  q(0)           = msg->pose.pose.orientation.w;
-  q(1)           = msg->pose.pose.orientation.x;
-  q(2)           = msg->pose.pose.orientation.y;
-  q(3)           = msg->pose.pose.orientation.z; 
+  q(0)           = msg.pose.pose.orientation.w;
+  q(1)           = msg.pose.pose.orientation.x;
+  q(2)           = msg.pose.pose.orientation.y;
+  q(3)           = msg.pose.pose.orientation.z; 
   pose.rows(3,5) = R_to_ypr(quaternion_to_R(q));
-  vel(0)         = msg->twist.twist.linear.x;       
-  vel(1)         = msg->twist.twist.linear.y;       
-  vel(2)         = msg->twist.twist.linear.z;       
+  vel(0)         = msg.twist.twist.linear.x;       
+  vel(1)         = msg.twist.twist.linear.y;       
+  vel(2)         = msg.twist.twist.linear.z;       
   // Drift Odom 
   static colvec drift_pose      = pose;
   static colvec drift_vel       = vel;   
   static colvec correction_pose = zeros<colvec>(6);  
   static colvec prev_pose       = pose;
-  static ros::Time prev_pose_t  = msg->header.stamp;  
+  static rclcpp::Time prev_pose_t  = msg.header.stamp;  
   if (config.enable_drift_odom)
   {
-    double dt       = (msg->header.stamp - prev_pose_t).toSec();
-    prev_pose_t     = msg->header.stamp;    
+    rclcpp::Time stamp(msg.header.stamp);
+    double dt = (stamp - prev_pose_t).seconds();
+    prev_pose_t = stamp;
     colvec d        = pose_update(pose_inverse(prev_pose), pose);
     prev_pose       = pose;
     d(0)           += (config.vdriftx   + config.stdvdriftxyz * as_scalar(randn(1))) * dt;    
@@ -120,67 +174,61 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
   noisy_odom.pose.pose.orientation.x = noisy_q(1);
   noisy_odom.pose.pose.orientation.y = noisy_q(2);
   noisy_odom.pose.pose.orientation.z = noisy_q(3);    
-  pubo.publish(noisy_odom);
+  pubo->publish(noisy_odom);
   // Check time interval and publish correction
-  static ros::Time prev_correction_t = msg->header.stamp;
-  if ((msg->header.stamp - prev_correction_t).toSec() > 1.0 / CORRECTION_RATE)
+  static rclcpp::Time prev_correction_t(msg.header.stamp);  // Declare and initialize once
+
+  rclcpp::Time stamp(msg.header.stamp);
+  if ((stamp - prev_correction_t).seconds() > 1.0 / CORRECTION_RATE)
   {
-    prev_correction_t             = msg->header.stamp;
-    correction.pose.position.x    = correction_pose(0);
-    correction.pose.position.y    = correction_pose(1);
-    correction.pose.position.z    = correction_pose(2);        
+    prev_correction_t = stamp;
+    correction.pose.position.x = correction_pose(0);
+    correction.pose.position.y = correction_pose(1);
+    correction.pose.position.z = correction_pose(2);
     colvec correction_q           = R_to_quaternion(ypr_to_R(correction_pose.rows(3,5)));
     correction.pose.orientation.w = correction_q(0);
     correction.pose.orientation.x = correction_q(1);
     correction.pose.orientation.y = correction_q(2);
     correction.pose.orientation.z = correction_q(3);   
-    pubc.publish(correction);         
+    pubc->publish(correction);         
   }
-}    
-
-void config_callback(so3_disturbance_generator::DisturbanceUIConfig &_config, uint32_t level) 
-{
-  config = _config;
 }
 
 void set_disturbance()
 {
-  geometry_msgs::Vector3 f;
-  geometry_msgs::Vector3 m;
+  geometry_msgs::msg::Vector3 f;
+  geometry_msgs::msg::Vector3 m;
   f.x = config.fxy  + config.stdfxy  * as_scalar(randn(1));
   f.y = config.fxy  + config.stdfxy  * as_scalar(randn(1));
   f.z = config.fz   + config.stdfz   * as_scalar(randn(1));    
   m.x = config.mrp  + config.stdmrp  * as_scalar(randn(1));    
   m.y = config.mrp  + config.stdmrp  * as_scalar(randn(1));    
   m.z = config.myaw + config.stdmyaw * as_scalar(randn(1));        
-  pubf.publish(f);
-  pubm.publish(m);
+  pubf->publish(f);
+  pubm->publish(m);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "so3_disturbance_generator");
-  ros::NodeHandle n("~");
+  rclcpp::init(argc, argv);
+  rclcpp::Node::SharedPtr n = std::make_shared<rclcpp::Node>("so3_disturbance_generator");
 
-  ros::Subscriber sub1 = n.subscribe("odom", 10, odom_callback);
-  pubo = n.advertise<nav_msgs::Odometry>(        "noisy_odom",         10);    
-  pubc = n.advertise<geometry_msgs::PoseStamped>("correction",         10);      
-  pubf = n.advertise<geometry_msgs::Vector3>(    "force_disturbance" , 10);
-  pubm = n.advertise<geometry_msgs::Vector3>(    "moment_disturbance", 10);  
-  
-  // Dynamic Reconfig
-  dynamic_reconfigure::Server<so3_disturbance_generator::DisturbanceUIConfig> server;
-  dynamic_reconfigure::Server<so3_disturbance_generator::DisturbanceUIConfig>::CallbackType ff;
-  ff = boost::bind(&config_callback, _1, _2);
-  server.setCallback(ff);   
+  declare_and_load_params(n);
 
-  ros::Rate r(100.0);
-  while(n.ok())
+  auto sub1 = n->create_subscription<nav_msgs::msg::Odometry>("odom", 10, &odom_callback);
+  pubo = n->create_publisher<nav_msgs::msg::Odometry>(        "noisy_odom",         10);    
+  pubc = n->create_publisher<geometry_msgs::msg::PoseStamped>("correction",         10);      
+  pubf = n->create_publisher<geometry_msgs::msg::Vector3>("force_disturbance" , 10);
+  pubm = n->create_publisher<geometry_msgs::msg::Vector3>("moment_disturbance", 10);
+
+  rclcpp::Rate r(100.0);
+  while(rclcpp::ok())
   {
-    ros::spinOnce();
+    rclcpp::spin_some(n);
     set_disturbance();
     r.sleep();
   }
 
+  rclcpp::shutdown();
   return 0;
 }
