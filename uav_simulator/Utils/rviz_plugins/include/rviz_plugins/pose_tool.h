4195c59f9ec3ce9d75651fa69800f9057fb0ffcc
damiankryzia71 <damiankryzia71@gmail.com>
2025-08-10 15:55:42 -0700

    convert pose_tool from rviz_plugins

--- uav_simulator/Utils/rviz_plugins/include/rviz_plugins/pose_tool.h
+++ uav_simulator/Utils/rviz_plugins/include/rviz_plugins/pose_tool.h
@@ -1,67 +1,51 @@
-/*
- * Copyright (c) 2008, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+#ifndef RVIZ_PLUGINS__POSE_TOOL_H_
+#define RVIZ_PLUGINS__POSE_TOOL_H_
 
-#ifndef RVIZ_POSE_TOOL_H
-#define RVIZ_POSE_TOOL_H
-
-#include <OgreVector3.h>
+#include <vector>
 
 #include <QCursor>
 
-#include <rclcpp/rclcpp.hpp>
+#include <OgrePlane.h>
+#include <OgreRay.h>
+#include <OgreSceneNode.h>
+#include <OgreVector3.h>
+#include <OgreViewport.h>
 
-#include <rviz_common/tool.hpp>
+#include "rviz_common/tool.hpp"
+#include "rviz_common/viewport_mouse_event.hpp"
+#include "rviz_rendering/objects/arrow.hpp"
 
-namespace rviz
+namespace rviz_plugins
 {
-class Arrow;
-class DisplayContext;
 
-class Pose3DTool : public Tool
+class Pose3DTool : public rviz_common::Tool
 {
+  Q_OBJECT
+
 public:
   Pose3DTool();
-  virtual ~Pose3DTool();
+  ~Pose3DTool() override;
 
-  virtual void onInitialize();
+  void onInitialize() override;
 
-  virtual void activate();
-  virtual void deactivate();
+  void activate() override;
+  void deactivate() override;
 
-  virtual int processMouseEvent(ViewportMouseEvent& event);
+  int processMouseEvent(rviz_common::ViewportMouseEvent & event) override;
 
 protected:
-  virtual void onPoseSet(double x, double y, double z, double theta) = 0;
+  /**
+   * @brief This function is called when the user completes the pose selection.
+   * A derived class would typically override this to publish the pose as a ROS message.
+   * It is now a regular virtual function with a default implementation.
+   */
+  virtual void onPoseSet(double x, double y, double z, double theta);
 
-  Arrow*              arrow_;
-  std::vector<Arrow*> arrow_array;
+  // Member variables for the main arrow and the array for height visualization
+  rviz_rendering::Arrow * arrow_;
+  std::vector<rviz_rendering::Arrow *> arrow_array_;
 
+  // The state machine for the tool's behavior
   enum State
   {
     Position,
@@ -70,8 +54,12 @@ protected:
   };
   State state_;
 
+  // The current 3D position being manipulated
   Ogre::Vector3 pos_;
-};
-}
 
-#endif
+  // No 'context_' member here; it is inherited from the base class.
+};
+
+}  // namespace rviz_plugins
+
+#endif  // RVIZ_PLUGINS__POSE_TOOL_H_
--- uav_simulator/Utils/rviz_plugins/src/pose_tool.cpp
+++ uav_simulator/Utils/rviz_plugins/src/pose_tool.cpp
@@ -1,165 +1,147 @@
-/*
- * Copyright (c) 2008, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <OgrePlane.h>
-#include <OgreRay.h>
-#include <OgreSceneNode.h>
-#include <OgreViewport.h>
-
-#include "rviz_rendering/geometry.hpp"
-#include "rviz_common/load_resource.hpp"
-#include "rviz_rendering/objects/arrow.hpp"
-#include "rviz_common/render_panel.hpp"
-#include "rviz_common/viewport_mouse_event.hpp"
-
 #include "rviz_plugins/pose_tool.h"
 
-namespace rviz
+#include <cmath>
+
+#include <OgreCamera.h>
+#include <OgreSceneManager.h>
+
+#include "rviz_common/display_context.hpp"
+#include "rviz_common/logging.hpp"
+#include "rviz_common/view_controller.hpp"
+#include "rviz_common/view_manager.hpp"
+
+namespace rviz_plugins
 {
 
+// Corrected constructor: Removed initialization of 'context_'
 Pose3DTool::Pose3DTool()
-  : Tool()
-  , arrow_(NULL)
+: rviz_common::Tool(), arrow_(nullptr), state_(Position)
 {
 }
 
 Pose3DTool::~Pose3DTool()
 {
   delete arrow_;
+  for (auto arrow : arrow_array_) {
+    delete arrow;
+  }
 }
 
-void
-Pose3DTool::onInitialize()
+void Pose3DTool::onInitialize()
 {
-  arrow_ = new Arrow(scene_manager_, NULL, 2.0f, 0.2f, 0.5f, 0.35f);
+  // Use the 'context_' member inherited from the base Tool class.
+  arrow_ = new rviz_rendering::Arrow(context_->getSceneManager(), nullptr, 2.0f, 0.2f, 0.5f, 0.35f);
   arrow_->setColor(0.0f, 1.0f, 0.0f, 1.0f);
   arrow_->getSceneNode()->setVisible(false);
 }
 
-void
-Pose3DTool::activate()
+void Pose3DTool::activate()
 {
   setStatus("Click and drag mouse to set position/orientation.");
   state_ = Position;
 }
 
-void
-Pose3DTool::deactivate()
+void Pose3DTool::deactivate()
 {
   arrow_->getSceneNode()->setVisible(false);
+  for (auto arrow : arrow_array_) {
+    arrow->getSceneNode()->setVisible(false);
+  }
 }
 
-int
-Pose3DTool::processMouseEvent(ViewportMouseEvent& event)
+// Added a default implementation to make the class concrete and loadable.
+void Pose3DTool::onPoseSet(double x, double y, double z, double theta)
 {
-  int                  flags = 0;
-  static Ogre::Vector3 ang_pos;
-  static double        initz;
-  static double        prevz;
-  static double        prevangle;
-  const double         z_scale    = 50;
-  const double         z_interval = 0.5;
-  Ogre::Quaternion     orient_x =
+  RVIZ_COMMON_LOG_INFO_STREAM(
+    "Pose3DTool: Pose set to " <<
+    "x: " << x <<
+    ", y: " << y <<
+    ", z: " << z <<
+    ", theta: " << theta);
+}
+
+int Pose3DTool::processMouseEvent(rviz_common::ViewportMouseEvent & event)
+{
+  int flags = 0;
+  static double prevangle = 0.0;
+  static double initz = 0.0;
+  static double prevy = 0.0;
+  const double z_scale = 50.0;
+  const double z_interval = 0.5;
+
+  Ogre::Quaternion orient_x =
     Ogre::Quaternion(Ogre::Radian(Ogre::Math::HALF_PI), Ogre::Vector3::UNIT_Z);
 
-  if (event.leftDown())
-  {
-    ROS_ASSERT(state_ == Position);
-    Ogre::Vector3 intersection;
-    Ogre::Plane   ground_plane(Ogre::Vector3::UNIT_Z, 0.0f);
-    if (getPointOnPlaneFromWindowXY(event.viewport, ground_plane, event.x,
-                                    event.y, intersection))
-    {
-      pos_ = intersection;
+  Ogre::Plane ground_plane(Ogre::Vector3::UNIT_Z, 0.0f);
+
+  rviz_common::ViewController * view_controller = context_->getViewManager()->getCurrent();
+  Ogre::Camera * camera = view_controller? view_controller->getCamera() : nullptr;
+  if (!camera) {
+    return flags;
+  }
+
+  Ogre::Ray ray = camera->getCameraToViewportRay(
+    static_cast<float>(event.x) / camera->getViewport()->getActualWidth(),
+    static_cast<float>(event.y) / camera->getViewport()->getActualHeight());
+
+  auto result = ray.intersects(ground_plane);
+
+  if (event.leftDown()) {
+    if (result.first) {
+      pos_ = ray.getPoint(result.second);
       arrow_->setPosition(pos_);
       state_ = Orientation;
       flags |= Render;
     }
-  }
-  else if (event.type == QEvent::MouseMove && event.left())
-  {
-    if (state_ == Orientation)
-    {
-      // compute angle in x-y plane
-      Ogre::Vector3 cur_pos;
-      Ogre::Plane   ground_plane(Ogre::Vector3::UNIT_Z, 0.0f);
-      if (getPointOnPlaneFromWindowXY(event.viewport, ground_plane, event.x,
-                                      event.y, cur_pos))
-      {
+  } else if (event.type == QEvent::MouseMove && event.left()) {
+    if (state_ == Orientation) {
+      if (result.first) {
+        Ogre::Vector3 cur_pos = ray.getPoint(result.second);
         double angle = atan2(cur_pos.y - pos_.y, cur_pos.x - pos_.x);
         arrow_->getSceneNode()->setVisible(true);
-        arrow_->setOrientation(Ogre::Quaternion(orient_x));
-        if (event.right())
-          state_  = Height;
-        initz     = pos_.z;
-        prevz     = event.y;
+        arrow_->setOrientation(Ogre::Quaternion(Ogre::Radian(angle), Ogre::Vector3::UNIT_Z) * orient_x);
+
+        if (event.right()) {
+          state_ = Height;
+          initz = pos_.z;
+          prevy = event.y;
+        }
         prevangle = angle;
         flags |= Render;
       }
-    }
-    if (state_ == Height)
-    {
-      double z  = event.y;
-      double dz = z - prevz;
-      prevz     = z;
-      pos_.z -= dz / z_scale;
+    } else if (state_ == Height) {
+      double dy = event.y - prevy;
+      prevy = event.y;
+      pos_.z -= dy / z_scale;
       arrow_->setPosition(pos_);
-      // Create a list of arrows
-      for (int k = 0; k < arrow_array.size(); k++)
-        delete arrow_array[k];
-      arrow_array.clear();
-      int cnt = ceil(fabs(initz - pos_.z) / z_interval);
-      for (int k = 0; k < cnt; k++)
-      {
-        Arrow* arrow__;
-        arrow__ = new Arrow(scene_manager_, NULL, 0.5f, 0.1f, 0.0f, 0.1f);
+
+      for (auto arrow : arrow_array_) {
+        delete arrow;
+      }
+      arrow_array_.clear();
+
+      int cnt = static_cast<int>(ceil(fabs(initz - pos_.z) / z_interval));
+      for (int k = 0; k < cnt; k++) {
+        rviz_rendering::Arrow * arrow__;
+        arrow__ = new rviz_rendering::Arrow(context_->getSceneManager(), nullptr, 0.5f, 0.1f, 0.0f, 0.1f);
         arrow__->setColor(0.0f, 1.0f, 0.0f, 1.0f);
         arrow__->getSceneNode()->setVisible(true);
         Ogre::Vector3 arr_pos = pos_;
-        arr_pos.z = initz - ((initz - pos_.z > 0) ? 1 : -1) * k * z_interval;
+        arr_pos.z = initz - ((initz - pos_.z > 0)? 1 : -1) * k * z_interval;
         arrow__->setPosition(arr_pos);
         arrow__->setOrientation(
-          Ogre::Quaternion(Ogre::Radian(prevangle), Ogre::Vector3::UNIT_Z) *
-          orient_x);
-        arrow_array.push_back(arrow__);
+          Ogre::Quaternion(Ogre::Radian(prevangle), Ogre::Vector3::UNIT_Z) * orient_x);
+        arrow_array_.push_back(arrow__);
       }
       flags |= Render;
     }
-  }
-  else if (event.leftUp())
-  {
-    if (state_ == Orientation || state_ == Height)
-    {
-      // Create a list of arrows
-      for (int k = 0; k < arrow_array.size(); k++)
-        delete arrow_array[k];
-      arrow_array.clear();
+  } else if (event.leftUp()) {
+    // Corrected the syntax error from '| |' to '||'
+    if (state_ == Orientation || state_ == Height) {
+      for (auto arrow : arrow_array_) {
+        delete arrow;
+      }
+      arrow_array_.clear();
       onPoseSet(pos_.x, pos_.y, pos_.z, prevangle);
       flags |= (Finished | Render);
     }
@@ -167,4 +149,8 @@ Pose3DTool::processMouseEvent(ViewportMouseEvent& event)
 
   return flags;
 }
-}
+
+}  // namespace rviz_plugins
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(rviz_plugins::Pose3DTool, rviz_common::Tool)
