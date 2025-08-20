ad26f632819f4d604e79ef271891f1ca69bbb058
damiankryzia71 <damiankryzia71@gmail.com>
2025-08-19 20:00:20 -0700

    create kino_launch.py, update topo_launch.py

--- src/plan_manage/launch/kino_launch.py
+++ src/plan_manage/launch/kino_launch.py
@@ -23,27 +23,27 @@ def generate_launch_description():
 
         # maximum velocity and acceleration the drone will reach
         DeclareLaunchArgument("max_vel", default_value="3.0"),
-        DeclareLaunchArgument("max_acc", default_value="2.5"),
+        DeclareLaunchArgument("max_acc", default_value="2.0"),
 
         # 1: use 2D Nav Goal to select goal
         # 2: use global waypoints below
-        # 3: use global waypoints below to set reference path
-        DeclareLaunchArgument("flight_type", default_value="2"),
+        DeclareLaunchArgument("flight_type", default_value="1"),
 
         # global waypoints
-        DeclareLaunchArgument("point_num", default_value="3"),
+        # If flight_type is set to 2, the drone will travel these waypoints one by one
+        DeclareLaunchArgument("point_num", default_value="2"),
 
         DeclareLaunchArgument("point0_x", default_value="19.0"),
         DeclareLaunchArgument("point0_y", default_value="0.0"),
         DeclareLaunchArgument("point0_z", default_value="1.0"),
 
-        # set more waypoints if you need
-        DeclareLaunchArgument("point1_x", default_value="19.0"),
-        DeclareLaunchArgument("point1_y", default_value="10.0"),
+        #set more waypoints if you need
+        DeclareLaunchArgument("point1_x", default_value="-19.0"),
+        DeclareLaunchArgument("point1_y", default_value="0.0"),
         DeclareLaunchArgument("point1_z", default_value="1.0"),
 
-        DeclareLaunchArgument("point2_x", default_value="-19.0"),
-        DeclareLaunchArgument("point2_y", default_value="0.0"),
+        DeclareLaunchArgument("point2_x", default_value="0.0"),
+        DeclareLaunchArgument("point2_y", default_value="19.0"),
         DeclareLaunchArgument("point2_z", default_value="1.0"),
 
         # planner manager node
@@ -61,12 +61,12 @@ def generate_launch_description():
             ],
             parameters=[
                 # replanning method
-                { "planner_node/planner": 2 },
+                { "planner_node/planner": 1 },
 
                 # planning fsm
                 {
                     "fsm/flight_type": ParameterValue(LaunchConfiguration("flight_type"), value_type=int),
-                    "fsm/thresh_replan": 0.5,
+                    "fsm/thresh_replan": 1.5,
                     "fsm/thresh_no_replan": 2.0,
 
                     "fsm/waypoint_num": ParameterValue(LaunchConfiguration("point_num"), value_type=int),
@@ -91,7 +91,7 @@ def generate_launch_description():
                     "sdf_map/local_update_range_y": 5.5,
                     "sdf_map/local_update_range_z": 4.5,
                     "sdf_map/obstacles_inflation": 0.099,
-                    "sdf_map/local_bound_inflate": 0.5,
+                    "sdf_map/local_bound_inflate": 0.0,
                     "sdf_map/local_map_margin": 50,
                     "sdf_map/ground_height": -1.0,
 
@@ -104,11 +104,11 @@ def generate_launch_description():
                     # depth filter
                     "sdf_map/use_depth_filter": True,
                     "sdf_map/depth_filter_tolerance": 0.15,
-                    "sdf_map/depth_filter_maxdist": 4.5,
+                    "sdf_map/depth_filter_maxdist": 5.0,
                     "sdf_map/depth_filter_mindist": 0.2,
                     "sdf_map/depth_filter_margin": 2,
                     "sdf_map/k_depth_scaling_factor": 1000.0,
-                    "sdf_map/skip_pixel": 3,
+                    "sdf_map/skip_pixel": 2,
 
                     # local fusion
                     "sdf_map/p_hit": 0.65,
@@ -120,8 +120,8 @@ def generate_launch_description():
                     "sdf_map/max_ray_length": 4.5,
 
                     "sdf_map/esdf_slice_height": 0.3,
-                    "sdf_map/visualization_truncate_height": 2.5,
-                    "sdf_map/virtual_ceil_height": 3.0,
+                    "sdf_map/visualization_truncate_height": 2.49,
+                    "sdf_map/virtual_ceil_height": 2.5,
                     "sdf_map/show_occ_time": False,
                     "sdf_map/show_esdf_time": False,
                     "sdf_map/pose_type": 1,
@@ -134,44 +134,41 @@ def generate_launch_description():
                     "manager/max_acc": ParameterValue(LaunchConfiguration("max_acc"), value_type=float),
                     "manager/max_jerk": 4.0,
                     "manager/dynamic_environment": 0,
-                    "manager/local_segment_length": 7.0,
+                    "manager/local_segment_length": 6.0,
                     "manager/clearance_threshold": 0.2,
-                    "manager/control_points_distance": 0.3,
+                    "manager/control_points_distance": 0.5,
 
                     "manager/use_geometric_path": False,
-                    "manager/use_kinodynamic_path": False,
-                    "manager/use_topo_path": True,
+                    "manager/use_kinodynamic_path": True,
+                    "manager/use_topo_path": False,
                     "manager/use_optimization": True
                 },
 
-                # topology path finding
+                # kinodynamic path finding
                 {
-                    "topo_prm/sample_inflate_x": 1.0,
-                    "topo_prm/sample_inflate_y": 3.5,
-                    "topo_prm/sample_inflate_z": 1.0,
-                    "topo_prm/clearance": 0.3,
-                    "topo_prm/max_sample_time": 0.005,
-                    "topo_prm/max_sample_num": 2000,
-                    "topo_prm/max_raw_path": 300,
-                    "topo_prm/max_raw_path2": 25,
-                    "topo_prm/short_cut_num": 1,
-                    "topo_prm/reserve_num": 6,
-                    "topo_prm/ratio_to_short": 5.5,
-                    "topo_prm/parallel_shortcut": True
+                    "search/max_tau": 0.6,
+                    "search/init_max_tau": 0.8,
+                    "search/max_vel": ParameterValue(LaunchConfiguration("max_vel"), value_type=float),
+                    "search/max_acc": ParameterValue(LaunchConfiguration("max_acc"), value_type=float),
+                    "search/w_time": 10.0,
+                    "search/horizon": 7.0,
+                    "search/lambda_heu": 5.0,
+                    "search/resolution_astar": 0.1,
+                    "search/time_resolution": 0.8,
+                    "search/margin": 0.2,
+                    "search/allocate_num": 100000,
+                    "search/check_num": 5
                 },
 
                 # trajectory optimization
                 {
                     "optimization/lambda1": 10.0,
                     "optimization/lambda2": 5.0,
-                    "optimization/lambda3": 0.0000,
-                    "optimization/lambda4": 0.001,
-                    "optimization/lambda5": 1.5,
-                    "optimization/lambda6": 10.0,
-                    "optimization/lambda7": 20.0,
+                    "optimization/lambda3": 0.00001,
+                    "optimization/lambda4": 0.01,
+                    "optimization/lambda7": 100.0,
 
                     "optimization/dist0": 0.4,
-                    "optimization/dist1": 0.0,
                     "optimization/max_vel": ParameterValue(LaunchConfiguration("max_vel"), value_type=float),
                     "optimization/max_acc": ParameterValue(LaunchConfiguration("max_acc"), value_type=float),
 
@@ -179,21 +176,17 @@ def generate_launch_description():
                     "optimization/algorithm2": 11,
                     "optimization/max_iteration_num1": 2,
                     "optimization/max_iteration_num2": 300,
+                    "optimization/max_iteration_num3": 200,
+                    "optimization/max_iteration_num4": 200,
                     "optimization/max_iteration_time1": 0.0001,
                     "optimization/max_iteration_time2": 0.005,
+                    "optimization/max_iteration_time3": 0.003,
+                    "optimization/max_iteration_time4": 0.003,
                     "optimization/order": 3,
 
                     "bspline/limit_vel": ParameterValue(LaunchConfiguration("max_vel"), value_type=float),
                     "bspline/limit_acc": ParameterValue(LaunchConfiguration("max_acc"), value_type=float),
-                    "bspline/limit_ratio": 1.1,
-
-                    "heading_planner/yaw_diff": 5 * 3.1415926 / 180.0,
-                    "heading_planner/half_vert_num": 3,
-                    "heading_planner/lambda1": 2.0,
-                    "heading_planner/lambda2": 1.0,
-                    "heading_planner/max_yaw_rate": 60 * 3.1415926 / 180.0,
-                    "heading_planner/w": 10.0,
-                    "fsm/act_map": False
+                    "bspline/limit_ratio": 1.1
                 }
             ]
         ),
